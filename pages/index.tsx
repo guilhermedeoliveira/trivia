de3e5775c6cf50f3a87dffe2ffc92c072054b389
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2021-03-27 11:22:56 -0300

    chore(setup): styled-components

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <header>header</header>
+
+    <main>main</main>
+
+    <footer>footer</footer>
+  </div>
+)
