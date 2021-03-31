616154312ec48796b0eabc1e2467dd822f400c7a
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2021-03-30 21:26:26 -0300

    feat(home): create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,78 +1,36 @@
-import { useState, useContext } from 'react'
-import { GetStaticProps } from 'next'
-import { useRouter } from 'next/router'
-import { equals, length } from 'ramda'
-
 import Page from 'shared/styleguide/components/Page'
-import QuestionCard from 'modules/quiz/components/QuestionCard'
+import styled from 'styled-components'
 
-import { AnsweredQuestionsContext } from 'shared/providers/AnsweredQuestionsProvider'
-import { normalizeQuizQuestions } from 'modules/quiz/lib/normalizers'
-import { ParsedResponse, Question } from 'shared/types'
+import { space, fontSize } from 'shared/styleguide/lib'
 
-type QuizProps = {
-  questions: Question[]
-}
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 100%;
+  padding: ${space('4')} ${space('4')};
+`
 
-const Quiz = ({ questions = [] }: QuizProps) => {
-  const { push } = useRouter()
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0)
-  const [completed, setCompleted] = useState<number>(0)
-  const { answeredQuestionsContext, setAnsweredQuestionsContext } = useContext(
-    AnsweredQuestionsContext
-  )
+const Text = styled.p`
+  font-size: ${fontSize('large')};
 
-  const currentQuestion = questions[currentQuestionIndex]
-
-  const onAnswerQuestion = (
-    optionAnswered: string
-  ): void | Promise<boolean> | NodeJS.Timeout => {
-    setAnsweredQuestionsContext((prevState: Question[]) => [
-      ...prevState,
-      { ...currentQuestion, optionAnswered },
-    ])
-
-    if (equals(length(answeredQuestionsContext), length(questions) - 1)) {
-      setCompleted((cur) => cur + 10)
-
-      return setTimeout(() => {
-        push('results')
-      }, 400)
-    }
-
-    setCompleted((cur) => cur + 10)
-    setCurrentQuestionIndex((cur) => cur + 1)
+  @media (min-width: 800px) {
+    font-size: ${fontSize('2xl')};
   }
+`
 
-  return (
-    <Page title="Trivia Game!">
-      <QuestionCard
-        question={currentQuestion}
-        onAnswerQuestion={onAnswerQuestion}
-        completed={completed}
-      />
-    </Page>
-  )
-}
+const Home = () => (
+  <Page
+    title="Welcome to the Trivia Challenge!"
+    navigation={{ text: 'begin', page: 'quiz' }}
+  >
+    <Wrapper>
+      <Text>You will be presented with 10 True of False questions.</Text>
 
-export default Quiz
+      <Text>Can you score 100?</Text>
+    </Wrapper>
+  </Page>
+)
 
-export const getStaticProps: GetStaticProps = async () => {
-  const rawResponse = await fetch(process.env.API_URL)
-  const parsedResponse = (await rawResponse.json()) as ParsedResponse
-
-  if (!parsedResponse?.results.length) {
-    return {
-      redirect: {
-        destination: '/error',
-        permanent: false,
-      },
-    }
-  }
-
-  return {
-    props: {
-      questions: normalizeQuizQuestions(parsedResponse.results),
-    },
-  }
-}
+export default Home
